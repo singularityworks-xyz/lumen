5319495c43275c5dc0edeec6cca8c8352452f7de
Harsh Sahu <yesh8harsh+github@gmail.com>
2025-12-22 03:11:37 +0530

    test[VERCEL]: vercel serverless functions

--- apps/workers/src/index.ts
+++ apps/workers/src/index.ts
@@ -1,9 +1,9 @@
-import { createLogger } from "@lumen/logger";
-import { Elysia } from "elysia";
+import { Elysia, t } from "elysia";
 
-const logger = createLogger();
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3002);
-
-logger.info(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-);
+export default new Elysia()
+  .get("/", () => "Hello Vercel Function")
+  .post("/", ({ body }) => body, {
+    body: t.Object({
+      name: t.String(),
+    }),
+  });
