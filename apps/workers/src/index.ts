5c4d3cfc7b430b5cccb18118c475479df63d99d2
Harsh Sahu <yesh8harsh+github@gmail.com>
2025-12-22 03:23:46 +0530

    off to selfhost

--- apps/workers/src/index.ts
+++ apps/workers/src/index.ts
@@ -1,9 +1,9 @@
-import { Elysia, t } from "elysia";
+import { createLogger } from "@lumen/logger";
+import { Elysia } from "elysia";
 
-export default new Elysia()
-  .get("/", () => "Hello Vercel Function")
-  .post("/", ({ body }) => body, {
-    body: t.Object({
-      name: t.String(),
-    }),
-  });
+const logger = createLogger();
+const app = new Elysia().get("/", () => "Hello Elysia").listen(3002);
+
+logger.info(
+  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+);
